6930c083116a60a52dc9534bc0df1a41b40486bc
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-02 21:58:24 +0500

    Adding: News

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import LanguageModels
 
 
-class RoomBookingSerializers(serializers.ModelSerializer):
+class LanguageSerializers(serializers.ModelSerializer):
     class Meta:
         model = LanguageModels
         fields = '__all__'
