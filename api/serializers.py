fcabd730f8b060db51ff630b82a7710945f24071
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 20:13:09 +0500

    Adding: New Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import LanguageModels
+from .models import LanguageModels, LanguageFileModels
 
 
 class LanguageSerializers(serializers.ModelSerializer):
     class Meta:
         model = LanguageModels
+        fields = '__all__'
+
+
+class LanguageFileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = LanguageFileModels
         fields = '__all__'
