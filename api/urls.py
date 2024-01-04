8f4cdbd42719a11c2269bad8b22e5fa3c52bf952
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 22:48:03 +0500

    Adding: Migration function

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LanguageCreateView, home_page, LanguageFileAddView
+from .views import LanguageCreateView, home_page, LanguageFileAddView, migration
 
 urlpatterns = [
     path('', home_page),
     path('add/', LanguageCreateView.as_view(), name='add'),
-    path('file/', LanguageFileAddView.as_view(), name='file')
-
+    path('file/', LanguageFileAddView.as_view(), name='file'),
+    path('api/migration', migration)
 ]
