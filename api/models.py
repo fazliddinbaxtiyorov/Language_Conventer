b593923d629ca29547445ffe08ab0ab521a50dab
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 20:12:32 +0500

    Adding: Some Models and New File models

--- api/models.py
+++ api/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 # Create your models here.
 class LanguageModels(models.Model):
-    text = models.TextField()
+    language_CHOICES = (
+        ('latin', 'latin'),
+        ('cyrillic', 'cyrillic'))
+    context = models.TextField()
+    pattern = models.CharField(max_length=20, choices=language_CHOICES)
+
+
+class LanguageFileModels(models.Model):
+    language_CHOICES = (
+        ('latin', 'latin'),
+        ('cyrillic', 'cyrillic'))
+    file = models.FileField(upload_to='file/', blank=True)
+    pattern = models.CharField(max_length=20, choices=language_CHOICES)
