317fd13a3d699aebdfb89c6ae0ed6f718b703d1b
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 22:41:52 +0500

    Adding: WhiteNoise Settings

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'file')
 MEDIA_URL = '/file/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
