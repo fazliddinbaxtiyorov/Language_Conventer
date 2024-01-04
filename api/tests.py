ce40a1ecd115b1f22cf7f140f1a2e5cd73737a37
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 23:36:16 +0500

    Add

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from .models import LanguageModels
+
 
 # Create your tests here.
+class LanguageConventerTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_home_page(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def testing_none_page(self):
+        response = self.client.get('/none/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_text_page(self):
+        self.client = APIClient()
+        response = self.client.get('/add/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def testing_files(self):
+        response = self.client.get('/file/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class TestModels(TestCase):
+    def setUp(self) -> None:
+        LanguageModels.objects.create(context='Salom', pattern='latin')
+        LanguageModels.objects.create(context='Xayr', pattern='cyrillic')
+        LanguageModels.objects.create(context='Эрталаб', pattern='latin')
+        LanguageModels.objects.create(context='Shaxmat', pattern='cyrillic')
+        LanguageModels.objects.create(context='лайлак', pattern='latin')
+
+    def test_context(self):
+        obj1 = LanguageModels.objects.get(context='Salom')
+        obj2 = LanguageModels.objects.get(context='Xayr')
+        obj3 = LanguageModels.objects.get(context='Эрталаб')
+        obj4 = LanguageModels.objects.get(context='Shaxmat')
+        self.assertEquals(obj1.context, "Salom")
+        self.assertEquals(obj2.context, "Xayr")
+        self.assertEquals(obj3.context, "Эрталаб")
+        self.assertEquals(obj4.context, "Shaxmat")
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,85 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from .models import LanguageModels, LanguageFileModels
+from .serializers import LanguageSerializers, LanguageFileSerializers
+from .latters import CYRILLIC_TO_LATIN, LATIN_TO_CYRILLIC
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'api/index.html')
+
+
+class LanguageCreateView(CreateAPIView):
+    queryset = LanguageModels
+    serializer_class = LanguageSerializers
+
+    def post(self, request):
+        data = request.data
+        if 'context' in data and 'pattern' in data:
+            context = data['context']
+            pattern = data['pattern']
+
+            if pattern == 'cyrillic':
+                result = ''
+                for i in context.replace('Sh', 'Ш').replace('Ch', 'Ч').replace('sh', 'ш').replace('ch', 'ч'):
+                    if i in CYRILLIC_TO_LATIN:
+                        result += CYRILLIC_TO_LATIN[i]
+                    else:
+                        result += i
+            elif pattern == 'latin':
+                result = ''
+                for i in context.replace('Ш', 'Sh').replace('Ч', 'Ch').replace('ш', 'sh').replace('ч', 'ch'):
+                    if i in LATIN_TO_CYRILLIC:
+                        result += LATIN_TO_CYRILLIC[i]
+                    else:
+                        result += i
+            else:
+                return Response({'error': 'Invalid pattern'})
+
+            return Response({'result': result})
+        else:
+            return Response({'error': 'Invalid request'})
+
+
+class LanguageFileAddView(CreateAPIView):
+    queryset = LanguageFileModels
+    serializer_class = LanguageFileSerializers
+
+    def post(self, request):
+        file = request.FILES['file']
+        pattern = request.data['pattern']
+
+        if file.name.endswith('.txt'):
+            content = file.read().decode('utf-8')
+            if pattern == 'cyrillic':
+                result = ''
+                for i in content.replace('Sh', 'Ш').replace('Ch', 'Ч').replace('sh', 'ш').replace('ch', 'ч'):
+                    if i in CYRILLIC_TO_LATIN:
+                        result += CYRILLIC_TO_LATIN[i]
+                    else:
+                        result += i
+            elif pattern == 'latin':
+                result = ''
+                for i in content.replace('Ш', 'Sh').replace('Ч', 'Ch').replace('ш', 'sh').replace('ч', 'ch'):
+                    if i in LATIN_TO_CYRILLIC:
+                        result += LATIN_TO_CYRILLIC[i]
+                    else:
+                        result += i
+            else:
+                return Response({'error': 'Invalid pattern'})
+
+            with open('result.txt', 'w', encoding='utf-8') as files:
+                files.write(result)
+
+            return Response({'result': 'File sent successfully, File Name: result.txt'})
+        else:
+            return Response({'error': 'Invalid File Format Not .txt File'})
+
+
+def migration(request):
+    import os
+    os.system('python3 manage.py makemigrations')
+    os.system('python3 manage.py migrate --no-input')
+    return HttpResponse('Migration Done')
