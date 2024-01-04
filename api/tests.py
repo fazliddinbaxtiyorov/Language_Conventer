86477fe1726e89854a294d0850f779a5060c9abe
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2024-01-04 22:53:48 +0500

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
 
 # Create your tests here.
+class LanguageConventerTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_home_page(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def testing_none_page(self):
+        response = self.client.get('/none/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_text_page(self):
+        self.client = APIClient()
+        response = self.client.get('/add/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def testing_files(self):
+        response = self.client.get('/file/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
