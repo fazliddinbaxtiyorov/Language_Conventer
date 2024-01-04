bb961a16da12c4c2aa81386e13cf10e8e8937ab3
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 22:36:25 +0500

    Upload: Home page view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LanguageCreateView, Home, LanguageFileAddView
+from .views import LanguageCreateView, home_page, LanguageFileAddView
 
 urlpatterns = [
-    path('', Home.as_view()),
-    path('add/', LanguageCreateView.as_view()),
-    path('file/', LanguageFileAddView.as_view())
+    path('', home_page),
+    path('add/', LanguageCreateView.as_view(), name='add'),
+    path('file/', LanguageFileAddView.as_view(), name='file')
 
 ]
