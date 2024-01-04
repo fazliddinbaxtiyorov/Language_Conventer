44a4c3e3191e451c7f5b8dd3d8cce00e174a95c2
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2024-01-04 23:26:32 +0500

    Adding: Home page view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from .models import LanguageModels, LanguageFileModels
+from .serializers import LanguageSerializers, LanguageFileSerializers
+from .latters import CYRILLIC_TO_LATIN, LATIN_TO_CYRILLIC
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'api/index.html')
+
