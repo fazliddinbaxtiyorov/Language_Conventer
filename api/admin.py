f560bfd57960fd02657fc0ff5a4e7037aa44235c
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2024-01-04 20:10:29 +0500

    Connecting: Models in Admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import LanguageModels, LanguageFileModels
 # Register your models here.
+admin.site.register(LanguageFileModels)
+admin.site.register(LanguageModels)
